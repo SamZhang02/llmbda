df0ac1abcbbeda2ab8669798db0252abf2549cb5
JeffDotPng <zhangjeff2014@gmail.com>
2023-12-11 22:58:23 -0500

    format with ruff

--- classes/LogicNode.py
+++ classes/LogicNode.py
@@ -2,21 +2,20 @@ from __future__ import annotations
 from typing import List, Dict
 from functools import reduce
 
-class LogicNode:
 
-    def __init__(self, value:str | List) -> None:
-        """Initialize a node with a list or a string; 
+class LogicNode:
+    def __init__(self, value: str | List) -> None:
+        """Initialize a node with a list or a string;
         Recursively points to its children if given a list"""
 
         if type(value) is str:
             self.value = value
             self.children = list()
             self.is_leaf = True
-        else: 
+        else:
             self.value = value[0]
             self.children = [LogicNode(child) for child in value[1:]]
             self.is_leaf = False
-
 
     def get_children(self) -> List[LogicNode]:
         return self.children
@@ -25,18 +24,17 @@ class LogicNode:
         return self.value
 
     def _get_leaves(self) -> List[str]:
-
         if self.is_leaf:
             return [self.value]
 
-        return reduce(lambda a,b: a + b, [child._get_leaves() for child in self.children])
-    
-    def _get_height(self) -> int:
+        return reduce(
+            lambda a, b: a + b, [child._get_leaves() for child in self.children]
+        )
 
+    def _get_height(self) -> int:
         if self.is_leaf:
             return 1
         return 1 + max([child._get_height() for child in self.children])
-
 
     def __str__(self) -> str:
         """Prints the node and its children for debugging, J
@@ -45,16 +43,26 @@ class LogicNode:
         if not self.children:
             return f"({self.value})"
 
-        return "(" + self.value + f" {reduce(lambda a,b: a+ ' ' + b, [child.__str__() for child in self.children])}" + ")"
+        return (
+            "("
+            + self.value
+            + f" {reduce(lambda a,b: a+ ' ' + b, [child.__str__() for child in self.children])}"
+            + ")"
+        )
 
     def _get_coursecodes(self) -> List[str]:
         if self.is_leaf:
             return [self.value]
 
-        return list(reduce(lambda a,b : a+b, [child._get_coursecodes() for child in self.children]))
+        return list(
+            reduce(
+                lambda a, b: a + b,
+                [child._get_coursecodes() for child in self.children],
+            )
+        )
 
-    def _compute_truth(self, rules:Dict[str, bool]) -> bool:
-        """Given a syntax tree node with course codes replaced to booleans, 
+    def _compute_truth(self, rules: Dict[str, bool]) -> bool:
+        """Given a syntax tree node with course codes replaced to booleans,
         compute whether the logical proposition is true of false based on its subtrees.
         """
 
@@ -62,11 +70,16 @@ class LogicNode:
             return rules[self.value]
 
         if self.value == "&":
-            return reduce(lambda a, b: a and b, [child._compute_truth(rules) for child in self.children])
+            return reduce(
+                lambda a, b: a and b,
+                [child._compute_truth(rules) for child in self.children],
+            )
         else:
-            return reduce(lambda a, b: a or b, [child._compute_truth(rules) for child in self.children])
+            return reduce(
+                lambda a, b: a or b,
+                [child._compute_truth(rules) for child in self.children],
+            )
+
 
 if __name__ == "__main__":
     node = LogicNode(["&", "COMP 202", ["|", "COMP 250", "COMP 206"]])
-
-
--- classes/LogicTree.py
+++ classes/LogicTree.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from classes.LogicNode import LogicNode
-from typing import List,Dict
+from typing import List, Dict
 from itertools import product
 from enum import Enum
 import re
@@ -8,21 +8,24 @@ import zss
 
 COURSE_CODE_PATTERN = re.compile(r"([A-Z0-9]{4} [0-9]{3}(?:D1|D2|N1|N2|J1|J2|J3)?)")
 
+
 class TreeComplexity(Enum):
     TRIVIAL = 1
     MEDIUM = 2
     HARD = 3
 
-class NotWellFormedTree(Exception): pass
+
+class NotWellFormedTree(Exception):
+    pass
+
 
 class LogicTree:
-
-    def __init__(self, root:LogicNode, lst:List):
+    def __init__(self, root: LogicNode, lst: List):
         self.root = root
         self.lst = lst
 
     @staticmethod
-    def is_well_formed(entity:List | str) -> bool:
+    def is_well_formed(entity: List | str) -> bool:
         """
         Method to check if a list if is a well formed logic tree before trying to
         instantiate it as the class.
@@ -47,23 +50,25 @@ class LogicTree:
                 return all([LogicTree.is_well_formed(child) for child in entity[1:]])
 
     @classmethod
-    def from_list(cls, lst:List) -> LogicTree:
+    def from_list(cls, lst: List) -> LogicTree:
         """
         Instantiate a tree from a list
         """
         if not cls.is_well_formed(lst):
-            raise NotWellFormedTree(f"Cannot instantiate LogicTreee, {lst} is not a well formed tree")
+            raise NotWellFormedTree(
+                f"Cannot instantiate LogicTreee, {lst} is not a well formed tree"
+            )
 
         return cls(LogicNode(lst), lst)
 
     def get_coursecodes(self) -> List[str]:
         return self.root._get_coursecodes()
 
-    def compute_truth(self, rules:Dict[str,bool]) -> bool:
+    def compute_truth(self, rules: Dict[str, bool]) -> bool:
         """Given a logic tree, compute whether the logical proposition is true of false based on a set of rules."""
         return self.root._compute_truth(rules)
 
-    def __eq__(self, other:LogicTree) -> bool:
+    def __eq__(self, other: LogicTree) -> bool:
         """Given two logic trees, compute whether they are logically equivalent."""
         self_course_codes = set(self.get_coursecodes())
         other_course_codes = set(other.get_coursecodes())
@@ -75,7 +80,7 @@ class LogicTree:
         all_rules_permutations = product([True, False], repeat=number_of_courses)
 
         for perm in all_rules_permutations:
-            rules = {k:v for k,v in zip(self_course_codes, perm)}
+            rules = {k: v for k, v in zip(self_course_codes, perm)}
             if self.compute_truth(rules) != other.compute_truth(rules):
                 return False
 
@@ -95,7 +100,7 @@ class LogicTree:
         number_of_leaves = len(self.get_leaves())
         height = self.get_height()
 
-        complexity_score = (number_of_leaves * height-1)
+        complexity_score = number_of_leaves * height - 1
 
         if complexity_score <= 4:
             return TreeComplexity.TRIVIAL
@@ -106,22 +111,28 @@ class LogicTree:
         else:
             return TreeComplexity.HARD
 
-
-    def distance_to(self,other:LogicTree, return_operations=False) -> int:
+    def distance_to(self, other: LogicTree, return_operations=False) -> int:
         """
         Method to compute the edit distance of two trees
         """
-        return zss.simple_distance(self.root, other.root,
-                                   get_children=LogicNode.get_children,
-                                   get_label=LogicNode.get_value,
-                                   return_operations=return_operations)
+        return zss.simple_distance(
+            self.root,
+            other.root,
+            get_children=LogicNode.get_children,
+            get_label=LogicNode.get_value,
+            return_operations=return_operations,
+        )
 
 
 if __name__ == "__main__":
     tree1 = LogicTree.from_list(["&", "COMP 202", ["|", "COMP 250", "COMP 206"]])
     tree2 = LogicTree.from_list(["&", "COMP 202", ["&", "COMP 250", "COMP 206"]])
-    tree3 = LogicTree.from_list(["|", ["&", "COMP 202", "COMP 250"], ["&", "COMP 202", "COMP 206"]])
-    tree4 = LogicTree.from_list(["|", ["&", "COMP 302", "COMP 250"], ["&", "COMP 202", "COMP 206"]])
+    tree3 = LogicTree.from_list(
+        ["|", ["&", "COMP 202", "COMP 250"], ["&", "COMP 202", "COMP 206"]]
+    )
+    tree4 = LogicTree.from_list(
+        ["|", ["&", "COMP 302", "COMP 250"], ["&", "COMP 202", "COMP 206"]]
+    )
 
     assert (tree1 == tree2) is False
     assert (tree1 == tree3) is True
@@ -133,16 +144,19 @@ if __name__ == "__main__":
     assert LogicTree.is_well_formed(["&", "COMP 202"]) is False
     assert LogicTree.is_well_formed(["&", "COMP 202", "COMP 250"]) is True
     assert LogicTree.is_well_formed(["&", "COMP 202J1", "COMP 250"]) is True
-    assert LogicTree.is_well_formed(["&", ["|", "COMP 202", "COMP 302"], "COMP 250"]) is True
+    assert (
+        LogicTree.is_well_formed(["&", ["|", "COMP 202", "COMP 302"], "COMP 250"])
+        is True
+    )
 
     assert tree1.distance_to(tree2) == 1
     assert tree3.distance_to(tree4) == 1
     assert tree1.distance_to(tree3) == 5
 
     assert sorted(tree1.get_leaves()) == sorted(["COMP 202", "COMP 250", "COMP 206"])
-    assert sorted(tree3.get_leaves()) == sorted(["COMP 202", "COMP 250", "COMP 202", "COMP 206"])
+    assert sorted(tree3.get_leaves()) == sorted(
+        ["COMP 202", "COMP 250", "COMP 202", "COMP 206"]
+    )
 
     assert tree1.get_height() == 3
     assert tree3.get_height() == 3
-
-
