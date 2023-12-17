0eb0dc13fef5b1644a4ffac9125f51508e330372
Liam <liam@scalzulli.com>
2023-12-16 21:56:46 -0500

    Re-run gemini experiment

--- experiments/gemini.py
+++ experiments/gemini.py
@@ -5,7 +5,16 @@ import google.generativeai as genai
 import pandas as pd
 from dotenv import load_dotenv
 
-PROMPT = "Given a list of course requirements, parse it into a logic expression tree structure. Each node is represented by an string or array. Non-leaf nodes are arrays, and represent nodes for logical operators. The first element in the array is a logical operator (AND represented by '&', OR being represented by '|'), the rest of the elements in the array are the children. Logical expressions should only ever be in the first element of the array. Leaf nodes are strings, never arrays. Leaf node values are the course codes in the requirements. These are only allowed to be valid course codes, not any arbitrary string. A valid course code is 4 uppercase/numeric characters followed by a space, then a 3 digit number. Use single quotes for strings."
+PROMPT = """
+Given a list of course requirements, parse it into a logic expression tree structure. Each node is represented by an
+string or array. Non-leaf nodes are arrays, and represent nodes for logical operators. The first element in the array is
+a logical operator (AND represented by '&', OR being represented by '|'), the rest of the elements in the array are the
+children. Logical expressions should only ever be in the first element of the array. Leaf nodes are strings, never
+arrays. Leaf node values are the course codes in the requirements. These are only allowed to be valid course codes, not
+any arbitrary string. A valid course code is 4 uppercase/numeric characters followed by a space, then a 3 digit number.
+Use single quotes for strings. Ignore anything words that are not valid course codes, like 'equivalent' or 'permission', etc.
+Also don't write AND or OR, use & for AND and | for OR always.
+"""
 
 
 class Model:
@@ -17,7 +26,7 @@ class Model:
         genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
         config = {
-            "temperature": 0.9,
+            "temperature": 0,
             "top_p": 1,
             "top_k": 1,
             "max_output_tokens": 2048,
@@ -69,6 +78,7 @@ class Model:
                 predictions.append(prediction)
             except Exception as e:
                 print(f"error: {e}")
+                predictions.append("")
                 continue
 
         out = {"index": dataset["index"], "predictions": predictions}
@@ -85,12 +95,12 @@ class Model:
             PROMPT
             + """
         Here's some examples, marked with input and expected output.
-    
+
         ---INPUT---
-        Prerequisite: PHGY 311   
+        Prerequisite: PHGY 311
         ---OUTPUT---
         'PHGY 311'
-    
+
         ---INPUT---
         Prerequisites: ECSE 205, COMP 206, ECSE 250, and (ECSE 343 or MATH 247) or equivalents.
         ---OUTPUT---
